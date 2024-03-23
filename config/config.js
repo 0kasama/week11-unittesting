9e93d4ed89442738ebd8917099cb8dedadcc4184
0kasama <okaajyad@gmail.com>
2024-03-23 21:06:46 +0700

    Initial Workflows

--- config/config.js
+++ config/config.js
@@ -11,8 +11,24 @@ module.exports = {
     test:{
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
+        database: process.env.DB_TEST,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+    },
+    docker: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: 'postgres',
+        port:5000
+    },
+    docker_test:{
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_TEST,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port:5000
     }
 }
